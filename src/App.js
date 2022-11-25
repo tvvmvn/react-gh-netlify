e8a29f82cbe75eb53cdd9726a1957100f153a363
tvvmvn <tvvmvn@gmail.com>
2022-11-25 14:05:02 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          REACT GIT NETLIFY
         </a>
       </header>
     </div>
